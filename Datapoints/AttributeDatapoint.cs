dd5114388f27571cbf06610439f6a49eec4acd8a
Michael Guldner <migu01@dfki.de>
2018-04-10 13:09:48 +0200

    added stub classes for datapoints

---
+++ Datapoints/AttributeDatapoint.cs
@@ -0,0 +1,15 @@
+﻿using LDPDatapoints.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LDPDatapoints;
+
+namespace ECA2LD.Datapoints
+{
+    class AttributeDatapoint<T>
+    {
+
+    }
+}
---
+++ Datapoints/EntityDatapoint.cs
@@ -0,0 +1,13 @@
+﻿using LDPDatapoints.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECA2LD.Datapoints
+{
+    class EntityDatapoint
+    {
+    }
+}
