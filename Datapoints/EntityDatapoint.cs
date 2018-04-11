a56404774a3ac9fbc1971826ca06d296fdf51553
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-04-11 12:56:24 +0200

    First implementation of Entity Datapoint

--- Datapoints/EntityDatapoint.cs
+++ Datapoints/EntityDatapoint.cs
@@ -1,4 +1,7 @@
-﻿using LDPDatapoints.Resources;
+﻿using ECA2LD.ldp_ttl;
+using ECABaseModel;
+using LDPDatapoints;
+using LDPDatapoints.Resources;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,31 @@ using System.Threading.Tasks;
 
 namespace ECA2LD.Datapoints
 {
-    class EntityDatapoint
+    public class EntityDatapoint : Resource
     {
+        EntityLDPGraph graph;
+
+        public EntityDatapoint(Entity value, string route) : base(route)
+        {
+            graph = new EntityLDPGraph(new Uri(route), value);
+        }
+
+        protected override void onGet(object sender, HttpEventArgs e)
+        {
+            string graphAsTTL = graph.GetTTL();
+            e.response.OutputStream.Write(Encoding.UTF8.GetBytes(graphAsTTL), 0, graphAsTTL.Length);
+            e.response.OutputStream.Flush();
+            e.response.OutputStream.Close();
+        }
+
+        protected override void onPost(object sender, HttpEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void onPut(object sender, HttpEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
