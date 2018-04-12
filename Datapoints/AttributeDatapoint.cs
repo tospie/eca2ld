ace6f68709a60dbf5319abd77915ce2f1ce88586
Michael Guldner <migu01@dfki.de>
2018-04-12 11:04:26 +0200

    implemented AttributeDatapoint

--- Datapoints/AttributeDatapoint.cs
+++ Datapoints/AttributeDatapoint.cs
@@ -5,11 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LDPDatapoints;
+using ECA2LD.ldp_ttl;
 
 namespace ECA2LD.Datapoints
 {
-    class AttributeDatapoint<T>
+    class AttributeDatapoint : Resource
     {
+        AttributeLDPGraph graph;
+        public AttributeDatapoint(ECABaseModel.Attribute attribute, string route) : base(route)
+        {
+            graph = new AttributeLDPGraph(new Uri(route), attribute);
+        }
 
+        protected override void onGet(object sender, HttpEventArgs e)
+        {
+            string graphAsTTL = graph.GetTTL();
+            e.response.OutputStream.Write(Encoding.UTF8.GetBytes(graphAsTTL), 0, graphAsTTL.Length);
+            e.response.OutputStream.Flush();
+            e.response.OutputStream.Close();
+        }
+
+        protected override void onPost(object sender, HttpEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void onPut(object sender, HttpEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
