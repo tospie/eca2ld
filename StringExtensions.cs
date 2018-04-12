ea95a462247e0e86b4d147c43b3ef1c2b3c1e2e7
Michael Guldner <migu01@dfki.de>
2018-04-12 11:05:20 +0200

    added string extension methods for easier access to the prototype uri

---
+++ StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECA2LD
+{
+    public static class StringExtensions
+    {
+        private static string getBaseUriString(this Uri u)
+        {
+            return u.Scheme + "://" + u.Host + ":" + u.Port + "/";
+        }
+
+        public static string getPrototypeBaseUri(this Uri u)
+        {
+            return u.getBaseUriString() + "prototypes/";
+        }
+    }
+}
