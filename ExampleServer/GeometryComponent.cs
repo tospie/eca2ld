7236d5220d8fdfc841e1501eeaaeb54730f0d7d1
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-21 09:36:13 +0200

    Added example components

---
+++ ExampleServer/GeometryComponent.cs
@@ -0,0 +1,21 @@
+﻿using ECABaseModel;
+using ECABaseModel.Prototypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleServer
+{
+    public static class GeometryComponent
+    {
+        public static void Register()
+        {
+            ComponentPrototype geometry = new ComponentPrototype("geometry");
+            geometry.AddAttribute<byte[]>("binaryMesh");
+            geometry.AddAttribute<string>("filetype");
+            ComponentRegistry.Instance.Register(geometry);
+        }
+    }
+}
---
+++ ExampleServer/SpatialEntityComponent.cs
@@ -0,0 +1,38 @@
+﻿using ECABaseModel;
+using ECABaseModel.Prototypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleServer
+{
+    public static class SpatialEntityComponent
+    {
+
+        public struct position
+        {
+            public double x;
+            public double y;
+            public double z;
+        };
+
+        public struct orientation
+        {
+            public double x;
+            public double y;
+            public double z;
+            public double w;
+        };
+
+        public static void RegisterComponents()
+        {
+            ComponentPrototype spatial = new ComponentPrototype("spatial");
+            spatial.AddAttribute<position>("position");
+            spatial.AddAttribute<orientation>("orientation");
+            spatial.AddAttribute<string>("id");
+            ComponentRegistry.Instance.Register(spatial);
+        }
+    }
+}
