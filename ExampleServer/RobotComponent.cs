e2029ee9066bbd8a945e436af8a856a5edca3545
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-22 11:09:45 +0200

    Added a robot comoponent stub

---
+++ ExampleServer/RobotComponent.cs
@@ -0,0 +1,21 @@
+﻿using ECABaseModel;
+using ECABaseModel.Prototypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleServer
+{
+    public static class RobotComponent
+    {
+        public static void Register()
+        {
+            ComponentPrototype robot = new ComponentPrototype("robot");
+            robot.AddAttribute<Entity>("arm");
+            robot.AddAttribute<Entity>("platform");
+            ComponentRegistry.Instance.Register(robot);
+        }
+    }
+}
