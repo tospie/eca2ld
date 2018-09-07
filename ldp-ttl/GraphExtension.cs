7482ebb7cda5fad59a2b7653a3b360eef97dccf2
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-09-07 10:01:57 +0200

    Graph Extension class that allows to copy entire graph

---
+++ ldp-ttl/GraphExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDS.RDF;
+
+namespace ECA2LD.ldp_ttl
+{
+    public static class GraphExtension
+    {
+        public static Graph CopyGraph(this Graph original)
+        {
+            Graph graphCopy = new Graph();
+            foreach (Triple t in original.Triples)
+            {
+                Triple copy = t.CopyTriple(graphCopy);
+                graphCopy.Assert(copy);
+            }
+            return graphCopy;
+        }
+    }
+}
